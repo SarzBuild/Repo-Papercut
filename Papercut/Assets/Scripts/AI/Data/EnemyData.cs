8a49284beb8f3120c86aa321223a3083f093e250
Tzunamis <35581476+Tzunamis@users.noreply.github.com>
2022-03-22 03:27:07 -0400

    Small Room & Code update
    Made some slight room adjustments, re-organized data list for AI stats.

--- Papercut/Assets/Scripts/AI/Data/EnemyData.cs
+++ Papercut/Assets/Scripts/AI/Data/EnemyData.cs
@@ -8,15 +8,15 @@ public class EnemyData : ScriptableObject
     public Node CurrentNode; //Used for animations
     public float IdleTime = 5f;
     public float PatrolTime = 5f;
-    public float CurrentVerticalSpeed, CurrentHorizontalSpeed;
-    public float FallClamped = -40f;
-    public float CurrentFallSpeed = 12f;
     public float ChaseRange = 8f;
     public float AttackRange = 1.3f;
-    public float MoveClamped = 8f;
-    public float Deceleration = 15f;
+    public float KnockbackSpeed = 2f;
     public float Acceleration = 10f;
+    public float Deceleration = 15f;
+    public float CurrentVerticalSpeed, CurrentHorizontalSpeed;
+    public float CurrentFallSpeed = 12f;
+    public float FallClamped = -40f;
+    public float MoveClamped = 8f;
     public float PatrolMoveClamped = 3f;
     public int IdlingState = 0;
-    public float KnockbackSpeed = 2f;
 }
