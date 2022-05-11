3c22fac42faf5b2c6d3047eb7b3a4737a1f349c5
SarzBuild <jrmietn@gmail.com>
2022-05-11 02:47:06 -0400

    Remastered boss end sequence

---
+++ Papercut/Assets/Scripts/Generic/LoadSceneCredits.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadSceneCredits : MonoBehaviour
+{
+    public void LoadCredits()
+    {
+        SceneManager.LoadScene("Credits");
+    }
+}
